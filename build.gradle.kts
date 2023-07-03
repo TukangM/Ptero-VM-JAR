4ab8307a8f9daf5b081a42345bc501a54e374dc9
AEP <91467886+TukangM@users.noreply.github.com>
2023-07-03 15:05:17 +0000

    idk

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,23 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(project.extra["java_version"].toString()))
 }
 
+// Configure the shadowJar task
 tasks.shadowJar {
-    dependencies {
-        include(dependency("org.yaml:snakeyaml:1.33"))
-    }
-}
-
-tasks.withType<Jar> {
+    archiveFileName.set("PteroVM-1.0.1-SNAPSHOT.jar")
+    destinationDirectory.set(file("$buildDir/libs"))
+    mergeServiceFiles()
     manifest {
         attributes("Main-Class" to "com.triassic.pterovm.Main")
     }
 }
+
+// Add a task to merge dependencies into the existing JAR
+task mergeDependencies(type: ShadowJar) {
+    from configurations.runtimeClasspath.get().filter { it.name.endsWith(".jar") }.map { zipTree(it) }
+    configurations = listOf(project.configurations.shadow)
+    archiveFileName.set("PteroVM-1.0.1-SNAPSHOT.jar")
+    destinationDirectory.set(file("$buildDir/libs"))
+}
+
+// Make the mergeDependencies task depend on shadowJar task
+mergeDependencies.dependsOn(tasks.shadowJar)
